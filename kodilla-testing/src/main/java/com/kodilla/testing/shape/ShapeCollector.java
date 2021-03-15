7a445cc1ed360495d4d29bfc37def287d033aec1
lparadylo <lukaszparadylo@gmail.com>
2021-03-15 16:39:07 +0100

    6.4 added classes: Circle, ShapeCollector, Square, Triangle, ShapeCollectorTestSuit; interface: Shape; added test implementation for ShapeCollector class

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector {
+
+    private List<Shape> figures = new ArrayList<>();
+
+    public boolean addFigure(Shape shape){
+        return false;
+    }
+    public boolean removeFigure(Shape shape){
+        return false;
+    }
+    public Shape getFigure(int n){
+        return null;
+    }
+    public void showFigures(){
+
+    }
+}
