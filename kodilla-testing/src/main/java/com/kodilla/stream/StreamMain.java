8ced5eb1cab3f4bc2b9b52fcddf6c73f3ae01f38
lparadylo <lukaszparadylo@gmail.com>
2021-03-19 16:42:41 +0100

    7.2. Added class ForumUser, created immutable fields

--- kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,8 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.immutable.ForumUser;
+import com.kodilla.stream.iterable.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.testing.shape.Circle;
 import com.kodilla.testing.shape.ShapeCollector;
@@ -9,10 +11,6 @@ import com.kodilla.testing.shape.Triangle;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("Cosik",a->a));
-        System.out.println(poemBeautifier.beautify("dlugopis",a->a));
-        System.out.println(poemBeautifier.beautify("monitor",a->a));
-        System.out.println(poemBeautifier.beautify("woda",a->a));
+        ForumUser forumUser = new ForumUser("Zdzichu", "Realny zdzichu");
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.immutable;
+
+public class ForumUser {
+    private final String username;
+    private final String realName;
+
+    public ForumUser(String username, String realName){
+        this.realName = realName;
+        this.username = username;
+    }
+}
