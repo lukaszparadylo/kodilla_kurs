da6eeb4e924f2752c33575243154924227777408
lparadylo <lukaszparadylo@gmail.com>
2021-03-20 16:53:12 +0100

    7.4. Added classes: Forum and ForumUser, performed stream operation

--- kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,31 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.immutable.ForumUser;
-import com.kodilla.stream.iterable.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.testing.shape.Circle;
-import com.kodilla.testing.shape.ShapeCollector;
-import com.kodilla.testing.shape.Square;
-import com.kodilla.testing.shape.Triangle;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
-    public static void main(String[] args) {
-        ForumUser forumUser = new ForumUser("Zdzichu", "Realny zdzichu");
-    }
+
+        public static void main(String[] args) {
+
+            Forum forum = new Forum();
+            Map<Integer, ForumUser> usersList = new HashMap<>();
+            usersList = forum.getUsers().stream()
+                    .filter(forumUser -> forumUser.getUserSex() == 'M')
+                    .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.of(2001,3,20)))
+                    .filter(forumUser -> forumUser.getNumberOfPosts()>0)
+                    .collect(Collectors.toMap(ForumUser::getUserId,forumUser -> forumUser));
+
+            System.out.println("# elements: " + usersList.size());
+            usersList.entrySet().stream()
+                    .map(entry -> "|Id: " +entry.getKey() + " |Name: " + entry.getValue().getUserName()+ " |User sex: "+entry.getValue().getUserSex()+
+                            " |Number of posts: "+entry.getValue().getNumberOfPosts()+ " |Date of birth: "+entry.getValue().getDateOfBirth()+"|")                   // [3]
+                    .forEach(System.out::println);
+        }
 }
