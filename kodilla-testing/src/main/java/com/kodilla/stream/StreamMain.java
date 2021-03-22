70f5379de081909e889e0d7ed12a3299e07ea4ed
lparadylo <lukaszparadylo@gmail.com>
2021-03-22 18:37:06 +0100

    7.3. fixed as per command "forum userow powinno dostac z zewnatrz poprzez konstruktor zamiast tworzyc sobie za kazdym razem taka sama liste" but ForumUsers are add to Forum by addForumUser() method

--- kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
@@ -11,10 +11,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
-
         public static void main(String[] args) {
 
             Forum forum = new Forum();
+            ForumUser forumUser1 = new ForumUser(0, "Jan", 'M',LocalDate.of(1990,1,1),2);
+            ForumUser forumUser2 = new ForumUser(1, "Wlodek", 'M',LocalDate.of(2001,1,1),3);
+            ForumUser forumUser3 = new ForumUser(2, "Andrzej", 'M',LocalDate.of(1976,1,1),0);
+            ForumUser forumUser4 = new ForumUser(3, "Aleksandra", 'K',LocalDate.of(2002,1,1),0);
+            ForumUser forumUser5 = new ForumUser(4, "Justyna", 'K',LocalDate.of(2013,1,1),4);
+            ForumUser forumUser6 = new ForumUser(5, "Anna", 'K',LocalDate.of(2000,1,1),5);
+            ForumUser forumUser7 = new ForumUser(6, "Zdzisław", 'M',LocalDate.of(1956,1,1),9);
+
+            forum.addForumUser(forumUser1);
+            forum.addForumUser(forumUser2);
+            forum.addForumUser(forumUser3);
+            forum.addForumUser(forumUser4);
+            forum.addForumUser(forumUser5);
+            forum.addForumUser(forumUser6);
+            forum.addForumUser(forumUser7);
+            
             Map<Integer, ForumUser> usersList = new HashMap<>();
             usersList = forum.getUsers().stream()
                     .filter(forumUser -> forumUser.getUserSex() == 'M')
--- kodilla-testing/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -7,19 +7,15 @@ import java.util.List;
 
 public class Forum {
 
-    private final List<ForumUser> users;
+    private List<ForumUser> users;
 
     public Forum(){
         this.users = new ArrayList<>();
-        this.users.add(new ForumUser(0, "Jan", 'M',LocalDate.of(1990,1,1),2));
-        this.users.add(new ForumUser(1, "Wlodek", 'M',LocalDate.of(2001,1,1),3));
-        this.users.add(new ForumUser(2, "Andrzej", 'M',LocalDate.of(1976,1,1),0));
-        this.users.add(new ForumUser(3, "Aleksandra", 'K',LocalDate.of(2002,1,1),0));
-        this.users.add(new ForumUser(4, "Justyna", 'K',LocalDate.of(2013,1,1),4));
-        this.users.add(new ForumUser(5, "Anna", 'K',LocalDate.of(2000,1,1),5));
-        this.users.add(new ForumUser(6, "Zdzisław", 'M',LocalDate.of(1956,1,1),9));
-    }
 
+    }
+    public void addForumUser(ForumUser forumUser){
+        this.users.add(forumUser);
+    }
     public List<ForumUser> getUsers() {
         return users;
     }
