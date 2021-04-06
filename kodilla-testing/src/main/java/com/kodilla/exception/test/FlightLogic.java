43d5ef4c3ff8566f773eda55a7ebfe94f0cb8d83
lparadylo <lukaszparadylo@gmail.com>
2021-04-06 12:56:52 +0200

    8.4. Created Classes: Flight, FlightLogic, RouteNotFoundException, Main. Was created new RouteNotFoundException and was called in Main class

---
+++ kodilla-testing/src/main/java/com/kodilla/exception/test/FlightLogic.java
@@ -0,0 +1,45 @@
+package com.kodilla.exception.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlightLogic {
+
+    Map<String, Boolean> airportAllowed;
+
+    public FlightLogic(List<Flight> flightList) {
+        airportAllowed = new HashMap<>();
+        for (Flight i : flightList){
+            this.airportAllowed.put(i.getArrivalAirport(), true);
+        }
+    }
+
+    public Boolean findFlight(Flight flight) throws RouteNotFoundException{
+        Boolean flyOrNot = false;
+
+        if(this.airportAllowed.containsKey(flight.getArrivalAirport())){
+            flyOrNot = this.airportAllowed.containsKey(flight.getArrivalAirport());
+        }
+        if(flyOrNot == false){
+            throw new RouteNotFoundException("There is no flight to "+flight.getArrivalAirport()+" ");
+        }
+        return flyOrNot;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FlightLogic that = (FlightLogic) o;
+
+        return airportAllowed.equals(that.airportAllowed);
+    }
+
+    @Override
+    public int hashCode() {
+        return airportAllowed.hashCode();
+    }
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kodilla.exception.test;
+
+public class RouteNotFoundException extends Exception{
+    RouteNotFoundException(final String message){
+        super(message);
+    }
+}
